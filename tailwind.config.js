b21a6d2c4f7c25861dae711aad89041077ee7a3d
Oliver Ni <oliver.ni@gmail.com>
2024-01-18 23:17:29 -0800

    update prose hr style

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
               marginTop: theme("spacing.16"),
               "&:first-child": { marginTop: 0 },
             },
+            hr: {
+              maxWidth: "50%",
+              borderStyle: "dotted",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginTop: theme("spacing.8"),
+              marginBottom: theme("spacing.8"),
+            },
             th: {
               borderRightWidth: theme("borderWidth.DEFAULT"),
               "&:last-child": { borderRightWidth: 0 },
@@ -25,10 +33,14 @@ module.exports = {
               fontWeight: "medium",
               color: theme("colors.blue.500"),
               transitionProperty: theme("transitionProperty.shadow"),
-              transitionTimingFunction: theme("transitionTimingFunction.in-out"),
+              transitionTimingFunction: theme(
+                "transitionTimingFunction.in-out"
+              ),
               transitionDuration: theme("transitionDuration.150"),
               boxShadow: `inset 0 -0.4em ${theme("colors.blue.100")}`,
-              "&:hover": { boxShadow: `inset 0 -0.6em ${theme("colors.blue.200")}`, },
+              "&:hover": {
+                boxShadow: `inset 0 -0.6em ${theme("colors.blue.200")}`,
+              },
             },
           },
         },
